a971301ac708051d59e23895babb13da1adcc843
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-14 22:02:36 -0500

    added first draft functionally to routes

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/todo', todoRouter);
--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,58 @@
 var express = require('express');
+var Joi = require('joi');
 var router = express.Router();
 
-router.get('/todolist/:userid', function (req, res, next) {
+var todoList = [
+    {
+        id: 0,
+        description: "this comes from the express app boii!",
+        done: false
+    },
+    {
+        id: 1,
+        description: "this comes from the express app boii again and again!",
+        done: false
+    },
+    {
+        id: 2,
+        description: "testing nodemon, one more time",
+        done: false
+    }
+];
+
+function validateTask (task) {
+    var schema = {
+        description: Joi.string().required(),
+    };
+    return Joi.validate(task, schema);
+}
+
+router.get('/todolist/:userid', function (req, res) {
     // get user's todo list with userid param
     // send todo list or error message
     res.json({
-        todoList: [
-            {
-                id: 1,
-                description: "this comes from the express app boii!",
-                done: false
-            },
-        ]
+        todoList: todoList
     });
 });
 
-router.put('/addtask/:task/:userid', function () {
+router.put('/addtask/:userid', function (req, res) {
     // get user's todo list with userid param
-    // add todo list item with task param
+    // add todo list item with task and userid param and description param
     // return success or error message.
+    var task = req.body;
+    var validatedTask = validateTask(task);
+    if (validatedTask.value) {
+        todoList.push({
+            id: todoList.length + 1,
+            description: task.description,
+            done: false
+        });
+        res.json({
+            todoList: todoList
+        });
+    } else {
+        //error message
+    }
 });
 
 router.put('/edittodo/:id/:userid/:description', function (req, res, next) {
