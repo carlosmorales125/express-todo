f5e145cd2e26ad5120d23a25749f4788dc257e7e
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-19 19:00:34 -0500

    now able to delete task and update information on mlab db

--- routes/todo.js
+++ routes/todo.js
@@ -8,23 +8,23 @@ router.get('/todolist/:userId', function (req, res) {
         userId: Joi.string().min(3).required()
     };
 
-    var validateUserId = Joi.validate({userId: req.params.userId}, schema);
+    Joi.validate({userId: req.params.userId}, schema)
+        .then(function () {
+            var userId = req.params.userId;
 
-    if (validateUserId.value) {
-        var userId = req.params.userId;
-
-        TodoList.find({ userId: userId })
-            .then(function (list) {
-                res.json({
-                    list: list
+            TodoList.find({ userId: userId })
+                .then(function (list) {
+                    res.json({
+                        list: list
+                    });
+                })
+                .catch(function (err) {
+                    res.status(500).send(err);
                 });
-            })
-            .catch(function (err) {
-                res.status(500).send(err);
-            });
-    } else {
-        res.status(400).send('The userId is required for this api call.');
-    }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
 });
 
 router.put('/addtask', function (req, res) {
@@ -33,53 +33,120 @@ router.put('/addtask', function (req, res) {
         description: Joi.string().min(3).required()
     };
 
-    var validatedTask = Joi.validate(req.body, schema);
+    Joi.validate(req.body, schema)
+        .then(function () {
+            var userId = req.body.userId;
+            var description = req.body.description;
 
-    if (validatedTask.value) {
-        var userId = req.body.userId;
-        var description = req.body.description;
+            var newTask = {
+                description: description,
+                done: false
+            };
 
-        var newTask = {
-            description: description,
-            done: false
-        };
-
-        TodoList.updateOne({ userId: userId }, { $push: { todoList: newTask } })
-            .then(function () {
-                res.sendStatus(200);
-            })
-            .catch(function (err) {
-                res.status(500).send(err);
-            });
-    } else {
-        res.status(400).send(validatedTask.error);
-    }
+            TodoList.updateOne({ userId: userId }, { $push: { todoList: newTask } })
+                .then(function () {
+                    res.sendStatus(200);
+                })
+                .catch(function (err) {
+                    res.status(500).send(err);
+                });
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
 });
 
-router.put('/edittodo', function (req, res, next) {
-    // get user's todo list with userid param
-    // edit todo list item with id param and description param
-    // return success or error message.
-    var id = req.body.id;
-    var description = req.body.description;
+router.put('/edittodo', function (req, res) {
+    var schema = {
+        userId: Joi.string().required(),
+        id: Joi.string().required(),
+        description: Joi.string().min(3).required()
+    };
 
-    todoList.forEach(item => {
-        if (item.id === id) {
-            item.description = description;
-        }
-    });
+    Joi.validate(req.body, schema)
+        .then(function () {
+            var userId = req.body.userId;
+            var id = req.body.id;
+            var description = req.body.description;
+
+            TodoList.updateOne({
+                    userId: userId,
+                    'todoList._id': id
+                },
+                {
+                    $set: { 'todoList.$.description': description }
+                })
+                .then(function () {
+                    res.sendStatus(200);
+                })
+                .catch(function (err) {
+                    res.status(500).send(err);
+                });
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
 });
 
-router.put('/completeorrestoretask/:id/:userid', function (req, res, next) {
-    // get user's todo list with userid param
-    // edit todo list item with id param
-    // return success or error message.
+router.put('/completeorrestoretask', function (req, res) {
+    var schema = {
+        userId: Joi.string().required(),
+        id: Joi.string().required(),
+        done: Joi.boolean().required()
+    };
+
+    Joi.validate(req.body, schema)
+        .then(function () {
+            var userId = req.body.userId;
+            var id = req.body.id;
+            var done = req.body.done;
+
+            TodoList.updateOne({
+                    userId: userId,
+                    'todoList._id': id
+                },
+                {
+                    $set: { 'todoList.$.done': done }
+                })
+                .then(function () {
+                    res.sendStatus(200);
+                })
+                .catch(function (err) {
+                    res.status(500).send(err);
+                });
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
 });
 
-router.delete('/deletetask/:id/:userid', function (req, res, next) {
-    // get user's todo list with userid param
-    // delete todo list item with id param
-    // return success or error message.
+router.delete('/deletetask/:id/:userId', function (req, res) {
+    var schema = {
+        userId: Joi.string().required(),
+        id: Joi.string().required()
+    };
+
+    Joi.validate(req.params, schema)
+        .then(function () {
+            var userId = req.params.userId;
+            var id = req.params.id;
+
+            TodoList.updateOne({
+                    userId: userId
+                },
+                {
+                    $pull: { todoList: { '_id': id } }
+                })
+                .then(function () {
+                    res.sendStatus(200);
+                })
+                .catch(function (err) {
+                    res.status(500).send(err);
+                });
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
 });
 
 module.exports = router;
