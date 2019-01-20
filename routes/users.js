7a8949052aeaa99e7d96eb9eb8af0e5370f052e1
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-20 15:48:29 -0500

    refactor of createuser

--- routes/users.js
+++ routes/users.js
@@ -1,87 +1,88 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const User = require('../models/User');
-const TodoList = require('../models/TodoList');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var Joi = require('joi');
+var User = require('../models/User');
+var TodoList = require('../models/TodoList');
 
 router.post('/createuser', function(req, res, next) {
-    // Grab variables from request body
-    var name = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
-    var confirmPassword = req.body.confirmPassword;
+    var schema = {
+        name: Joi.string().min(2).required(),
+        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        password: Joi.string().min(6).required(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+    };
 
-    // Create Error Array
-    var errors = [];
+    Joi.validate(req.body, schema)
+        .then(function () {
+            // Grab variables from request body
+            var name = req.body.name;
+            var email = req.body.email;
+            var password = req.body.password;
 
-    // Todo: validate with joi or something else.
-    // Validate data and create errors if not valid
-    if (!name || !email || !password || !confirmPassword) {
-        errors.push({ msg: 'Please enter all fields' });
-    }
-
-    if (password !== confirmPassword) {
-        errors.push({ msg: 'Passwords do not match' });
-    }
-
-    if (password.length < 6) {
-        errors.push({ msg: 'Password must be at least 6 characters' });
-    }
-
-    if (errors.length > 0) {
-        // lets send a message with errors
-        res.status(400).send(errors);
-    } else {
-        // Make sure no other users have entered this emai;
-        User.findOne({email: email}).then(function (user) {
-            if (user) {
-                errors.push({msg: 'Email already exists'});
-                res.status(409).send(errors);
-            } else {
-                // init new user
-                var newUser = new User({
-                    name: name,
-                    email: email,
-                    password: password
-                });
-
-                bcrypt.genSalt(10, function (err, salt) {
-                    bcrypt.hash(newUser.password, salt, function (err, hash) {
-                        if (err) {
-                            res.send(500).send('internal error');
-                            throw err;
-                        }
-                        newUser.password = hash;
-                        newUser
-                            .save()
-                            .then(function (user) {
-                                //create the user's first todo list
-                                var newTodoList = new TodoList({
-                                    userId: user._id
-                                });
-                                newTodoList
-                                    .save()
-                                    .then(function (todoList) {
-                                        console.log(todoList);
-                                        res.send(user);
-                                    })
-                                    .catch(function (err) {
-                                        res.status(500).send(err);
-                                    });
-                            })
-                            .catch(function (err) {
-                                res.status(500).send(err)
-                            });
+            // Make sure no other users have entered this email;
+            User.findOne({email: email})
+                .then(function (user) {
+                if (user) {
+                    res.status(409).send('Email already exists');
+                } else {
+                    // init new user
+                    var newUser = new User({
+                        name: name,
+                        email: email,
+                        password: password
                     });
-                });
-            }
+                    bcrypt.genSalt(10, function (err, salt) {
+                        bcrypt.hash(newUser.password, salt, function (err, hash) {
+                            if (err) {
+                                res.send(500).send('internal error');
+                                throw err;
+                            }
+                            newUser.password = hash;
+                            newUser
+                                .save()
+                                .then(function (user) {
+                                    //create the user's first todo list
+                                    console.log('this is a new user', user);
+                                    var newTodoList = new TodoList({
+                                        userId: user._id
+                                    });
+                                    newTodoList
+                                        .save()
+                                        .then(function (todoList) {
+                                            res.send(user);
+                                        })
+                                        .catch(function (err) {
+                                            res.status(500).send(err);
+                                        });
+                                })
+                                .catch(function (err) {
+                                    res.status(500).send(err)
+                                });
+                        });
+                    });
+                }
+            });
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
         });
-    }
 });
 
 router.post('/auth', function (req, res, next) {
-    
+    var schema = {
+        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        password: Joi.string().min(6).required()
+    };
+
+    Joi.validate(req.body, schema)
+        .then(function (value) {
+
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        })
 });
 
 module.exports = router;
