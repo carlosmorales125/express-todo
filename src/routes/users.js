ec70b409f5edcbddab3ccfee58bc03e65c482471
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-23 21:11:29 -0500

    converting users routes to use async await and try catch blocks

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,133 +8,105 @@ import config from '../config';
 
 const router = express.Router();
 
-router.post('/createuser', (req, res) => {
-    const schema = {
-        name: Joi.string().min(2).required(),
-        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-        password: Joi.string().min(6).required(),
-        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
-    };
+router.post('/createuser', async function(req, res) {
+    try {
+        const schema = {
+            name: Joi.string().min(2).required(),
+            email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+            password: Joi.string().min(6).required(),
+            confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+        };
 
-    Joi.validate(req.body, schema)
-        .then(() => {
-            // Grab variables from request body
-            const { name, email, password } = req.body;
+        await Joi.validate(req.body, schema);
 
-            // Make sure no other users have entered this email;
-            User.findOne({email: email})
-                .then(user => {
-                if (user) {
-                    res.status(409).send('Email already exists');
-                } else {
-                    // init new user
-                    let newUser = new User({
-                        name: name,
-                        email: email,
-                        password: password,
-                        token: 'fakefornow'
-                    });
-                    newUser
-                        .save()
-                        .then(user => {
-                            //create the user's first todo list
-                            let newTodoList = new TodoList({
-                                userId: user._id
-                            });
-                            newTodoList
-                                .save()
-                                .then(() => {
-                                    res.sendStatus(200);
-                                })
-                                .catch(err => {
-                                    res.status(500).send(err);
-                                });
-                        })
-                        .catch(err => {
-                            res.status(500).send(err);
-                        });
-                }
-            });
-        })
-        .catch(err => {
-            res.status(400).send(err);
+        const { name, email, password } = req.body;
+
+        const currentUser = await User.findOne({email: email});
+
+        if (currentUser) return res.status(409).send('Email already exists');
+
+        // Init new user
+        let newUser = new User({
+            name: name,
+            email: email,
+            password: password,
+            token: 'fakefornow'
         });
+
+        const newlyCreatedUser = await newUser.save();
+
+        // Create the user's first todo list
+        let newTodoList = new TodoList({
+            userId: newlyCreatedUser._id
+        });
+
+        await newTodoList.save();
+
+        res.sendStatus(200);
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
-router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (err, user) => {
-        if (err || !user) {
-            return res.status(400).send(err);
-        }
-        req.login(user, {session: false}, err => {
-            if (err) {
-                res.status(500).send(err);
-            }
+router.post('/login', async function(req, res) {
+    try {
+        passport.authenticate('local', { session: false }, (e, user) => {
+            if (e || !user) return res.status(400).send(e);
 
-            const token = jwt.sign(user.toJSON(), config.secret, {
-                expiresIn: 86400
-            });
+            req.login(user, {session: false}, async function(e) {
+                if (e) return res.status(500).send(e);
 
-            const _id = user.get('_id');
-
-            User.updateOne({_id: _id}, { $set: { token: token } })
-                .then(() => {
-                    // everything but the password
-                    let userObject = {
-                        _id: _id,
-                        name: user.get('name'),
-                        email: user.get('email'),
-                        token: token
-                    };
-
-                    return res.json(userObject);
-                })
-                .catch(err => {
-                    res.status(500).send(err);
+                const token = jwt.sign(user.toJSON(), config.secret, {
+                    expiresIn: 86400
                 });
-        });
-    })(req, res);
+
+                const _id = user.get('_id');
+
+                await User.updateOne({_id: _id}, { $set: { token: token } });
+
+                let userObject = {
+                    _id: _id,
+                    name: user.get('name'),
+                    email: user.get('email'),
+                    token: token
+                };
+
+                return res.json(userObject);
+            });
+        })(req, res);
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
-router.post('/changename', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const schema = {
-        description: Joi.string().min(2).required(),
-        id: Joi.string().min(6).required()
-    };
+router.post('/changename', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        const schema = {
+            description: Joi.string().min(2).required(),
+            id: Joi.string().min(6).required()
+        };
 
-    Joi.validate(req.body, schema)
-        .then(() => {
-            const { name, id } = req.body;
+        await Joi.validate(req.body, schema);
 
-            User.updateOne({
-                _id: id
-                },
-                {
-                    $set: { name: name }
-                })
-                .then(() => {
-                    User.findOne({ _id: id })
-                        .then(user => {
-                            // everything but the password
-                            let userObject = {
-                                _id: user.get('_id'),
-                                name: user.get('name'),
-                                email: user.get('email'),
-                                token: user.get('token')
-                            };
-                            res.status(200).json(userObject);
-                        })
-                        .catch(err => {
-                            res.status(500).send(err);
-                        });
-                })
-                .catch(err => {
-                    res.status(500).send(err);
-                });
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+        const { description, id } = req.body;
+
+        const updatedUserResult = await User.updateOne({ _id: id }, { $set: { name: description } });
+
+        if (updatedUserResult.nModified !== 1) return res.status(500).send('Unable to modify user.');
+
+        const updatedUser = await User.findOne({ _id: id });
+
+        let userObject = {
+            _id: updatedUser.get('_id'),
+            name: updatedUser.get('name'),
+            email: updatedUser.get('email'),
+            token: updatedUser.get('token')
+        };
+
+        res.status(200).json(userObject);
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
 export { router as usersRouter };
