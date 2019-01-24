3d426b471405b0663340d40af0a4b0704e6f3e7c
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-23 20:00:06 -0500

    converting todo routes to use async await and try catch blocks

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -2,153 +2,110 @@ import express from 'express';
 import Joi from 'joi';
 import passport from 'passport';
 import TodoList from '../models/TodoList';
-
 const router = express.Router();
 
+router.get('/todolist/:userId', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        const schema = {
+            userId: Joi.string().min(3).required()
+        };
 
-router.get('/todolist/:userId', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const schema = {
-        userId: Joi.string().min(3).required()
-    };
+        await Joi.validate({userId: req.params.userId}, schema);
 
-    Joi.validate({userId: req.params.userId}, schema)
-        .then(() => {
-            const { userId } = req.params;
+        const { userId } = req.params;
 
-            TodoList.find({ userId: userId })
-                .then(list => {
-                    res.json({
-                        list: list
-                    });
-                })
-                .catch(err => {
-                    res.status(500).send(err);
-                });
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+        const list = await TodoList.find({ userId: userId });
+
+        res.json({ list });
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
-router.put('/addtask', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const schema = {
-        userId: Joi.string().required(),
-        description: Joi.string().min(3).required()
-    };
+router.put('/addtask', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        const schema = {
+            userId: Joi.string().required(),
+            description: Joi.string().min(3).required()
+        };
 
-    Joi.validate(req.body, schema)
-        .then(() => {
-            const userId = req.body.userId;
-            const description = req.body.description;
+        await Joi.validate(req.body, schema);
 
-            const newTask = {
-                description: description,
-                done: false
-            };
+        const { userId, description } = req.body;
 
-            TodoList.updateOne({ userId: userId }, { $push: { todoList: newTask } })
-                .then(() => {
-                    res.sendStatus(200);
-                })
-                .catch(err => {
-                    res.status(500).send(err);
-                });
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+        const newTask = {
+            description: description,
+            done: false
+        };
+
+        await TodoList.updateOne({ userId: userId }, { $push: { todoList: newTask } });
+
+        res.sendStatus(200);
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
-router.put('/edittodo', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const schema = {
-        userId: Joi.string().required(),
-        id: Joi.string().required(),
-        description: Joi.string().min(3).required()
-    };
+router.put('/edittodo', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        const schema = {
+            userId: Joi.string().required(),
+            id: Joi.string().required(),
+            description: Joi.string().min(3).required()
+        };
 
-    Joi.validate(req.body, schema)
-        .then(() => {
-            const userId = req.body.userId;
-            const id = req.body.id;
-            const description = req.body.description;
+        await Joi.validate(req.body, schema);
 
-            TodoList.updateOne({
-                    userId: userId,
-                    'todoList._id': id
-                },
-                {
-                    $set: { 'todoList.$.description': description }
-                })
-                .then(() => {
-                    res.sendStatus(200);
-                })
-                .catch(err => {
-                    res.status(500).send(err);
-                });
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+        const { userId, id, description } = req.body;
+
+        await TodoList.updateOne({ userId: userId, 'todoList._id': id},
+            { $set: { 'todoList.$.description': description } });
+
+        res.sendStatus(200);
+
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
-router.put('/completeorrestoretask', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const schema = {
-        userId: Joi.string().required(),
-        id: Joi.string().required(),
-        done: Joi.boolean().required()
-    };
+router.put('/completeorrestoretask', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        const schema = {
+            userId: Joi.string().required(),
+            id: Joi.string().required(),
+            done: Joi.boolean().required()
+        };
 
-    Joi.validate(req.body, schema)
-        .then(() => {
-            const userId = req.body.userId;
-            const id = req.body.id;
-            const done = req.body.done;
+        await Joi.validate(req.body, schema);
 
-            TodoList.updateOne({
-                    userId: userId,
-                    'todoList._id': id
-                },
-                {
-                    $set: { 'todoList.$.done': done }
-                })
-                .then(() => {
-                    res.sendStatus(200);
-                })
-                .catch(err => {
-                    res.status(500).send(err);
-                });
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+        const { userId, id, done } = req.body;
+
+        await TodoList.updateOne({ userId: userId, 'todoList._id': id },
+            { $set: { 'todoList.$.done': done } });
+
+        res.sendStatus(200);
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
-router.delete('/deletetask/:id/:userId', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const schema = {
-        userId: Joi.string().required(),
-        id: Joi.string().required()
-    };
+router.delete('/deletetask/:id/:userId', passport.authenticate('jwt', { session: false }), async function(req, res) {
+    try {
+        const schema = {
+            userId: Joi.string().required(),
+            id: Joi.string().required()
+        };
 
-    Joi.validate(req.params, schema)
-        .then(() => {
-            const { userId, id } = req.body;
+        await Joi.validate(req.params, schema);
 
-            TodoList.updateOne({
-                    userId: userId
-                },
-                {
-                    $pull: { todoList: { '_id': id } }
-                })
-                .then(() => {
-                    res.sendStatus(200);
-                })
-                .catch(err => {
-                    res.status(500).send(err);
-                });
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+        const { userId, id } = req.params;
+
+        await TodoList.updateOne({ userId: userId }, { $pull: { todoList: { '_id': id } } });
+
+        res.sendStatus(200);
+    } catch(e) {
+        res.status(400).send(e);
+    }
 });
 
 export { router as todoRouter };
